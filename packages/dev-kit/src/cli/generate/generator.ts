9dc2f8bc0b1d8a1f88c7f23b818dd5aaf3a8521a
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2024-08-27 05:05:26 -0600

    refactor(dev-kit): updated Generator class and their generate method moving params from method to constructor

--- packages/dev-kit/src/cli/generate/generator.ts
+++ packages/dev-kit/src/cli/generate/generator.ts
@@ -2,17 +2,26 @@ import {mkdirSync, readFileSync} from "fs";
 import {render} from "ejs";
 import {existsSync, writeFileSync} from "node:fs";
 
-export class Generator {
+export interface GeneratorParams {
+  templatesPath: string;
+  templatesListToGenerate: { templateName: string, extension: string }[];
+}
+
+export abstract class Generator implements GeneratorParams {
 
   private readonly _path: string;
+  public readonly templatesPath: string;
+  public readonly templatesListToGenerate: { templateName: string, extension: string }[];
 
-  constructor(path: string) {
+  protected constructor(path: string, { templatesPath, templatesListToGenerate }: GeneratorParams) {
     this._path = path;
+    this.templatesPath = templatesPath;
+    this.templatesListToGenerate = templatesListToGenerate;
   }
 
-  public generate<T extends Object>(templatesPath: string, templatesListToGenerate: { templateName: string, extension: string }[], data: T): void {
-    for (const templateItem of templatesListToGenerate) {
-      const templateContent: string = readFileSync(templatesPath.concat(`/${templateItem.templateName}.ejs`), { encoding: "utf-8", flag: 'r' });
+  public generate<T extends Object>(data: T): void {
+    for (const templateItem of this.templatesListToGenerate) {
+      const templateContent: string = readFileSync(this.templatesPath.concat(`/${templateItem.templateName}.ejs`), { encoding: "utf-8", flag: 'r' });
       const docContent: string = render(templateContent, data, {});
       if (!existsSync(this._path)) {
         mkdirSync(this._path, { recursive: true });
