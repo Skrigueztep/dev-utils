896f05d861e0724b2791a78ae3384be010cadf17
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2024-08-27 05:12:15 -0600

    feat(dev-kit): added State for CLI

---
+++ packages/dev-kit/src/cli/cli/state.ts
@@ -0,0 +1,23 @@
+import {State as IState} from "../core/base/cli";
+import {ConsoleArgs} from "../core/utils/get-console-agrs";
+import {resolve} from "path";
+import {cwd} from "process";
+import {GeneratorsEnum} from "./generators-enum";
+
+interface StateDefinition {
+  generate: string;
+  path: string;
+}
+
+export class State extends IState implements StateDefinition {
+
+  public generate: GeneratorsEnum;
+  public path: string;
+
+  constructor(args?: ConsoleArgs) {
+    super();
+    this.generate = (args && args['generate']) ? args['generate'] as GeneratorsEnum : GeneratorsEnum.VUE;
+    this.path = (args && args['path']) ? resolve(cwd(), args['path'] as string) : resolve(cwd(), '');
+  }
+
+}
