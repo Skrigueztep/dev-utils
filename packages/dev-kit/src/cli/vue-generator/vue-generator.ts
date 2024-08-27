4a6e08457ce6681003e06a00875056c68ac2a486
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2024-08-27 05:05:53 -0600

    feat(dev-kit): added Vue Generator

---
+++ packages/dev-kit/src/cli/vue-generator/vue-generator.ts
@@ -0,0 +1,16 @@
+import {Generator, GeneratorParams} from "../generate/generator";
+import {join} from "path";
+
+export class VueGenerator extends Generator implements GeneratorParams {
+
+  constructor(path: string) {
+    super(path, {
+      templatesPath: join(__dirname, '../assets/templates/vue'),
+      templatesListToGenerate: [
+        { templateName: 'component', extension: '.vue' },
+        { templateName: 'component.test', extension: '.ts' }
+      ]
+    });
+  }
+
+}
