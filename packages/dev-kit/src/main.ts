6d27704dc6a35a2611aa27b080d0bc334dbb04d8
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2024-08-27 05:39:08 -0600

    feat(dev-kit): added available commands at empty args

--- packages/dev-kit/src/main.ts
+++ packages/dev-kit/src/main.ts
@@ -6,6 +6,20 @@ import {State} from "./cli/cli/state";
 import {Main} from "./cli/cli/main";
 
 const args: ConsoleArgs = getConsoleArgs(process.argv);
+
+if (Object.keys(args).length === 0) {
+
+  console.log(`
+  Available commands
+  
+  --generator   Enable generator files
+    --path        Output path for files with filename included  - Required for generator
+  --version     Prints current module version
+  `);
+
+  process.exit(0);
+}
+
 const state: State = new State(args);
 const cli: CLI<State> = new Main(state);
 cli.execute();
